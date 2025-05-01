9790d7e6bd4047aa2fed97a20a37d91500520e4a
Lucas Carvalho <barraroot@gmail.com>
2025-05-01 02:37:40 -0300

    Ajust token tenant

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,3 +1,3 @@
 // Constantes da API externa
 export const EXTERNAL_API_BASE_URL = 'https://backoffice.nextradeglobal.io/api/v1';
-export const REGISTER_TOKEN = '2eb1dc49-cd9a-4408-a871-902cffd9ce1c';
+export const REGISTER_TOKEN = 'e7da13c4-1a61-4d89-9af5-fad6df2100e5';
