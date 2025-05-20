4cb07cc7e10910afeb0e17fed1947d5f4d2f5f39
danoliveira <daniel.oliveira.blv.13@gmail.com>
2025-05-20 19:30:06 -0300

    Changed url api

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,3 +1,3 @@
 // Constantes da API externa
-export const EXTERNAL_API_BASE_URL = 'https://backoffice.dubbinary.com/api/v1';
-export const REGISTER_TOKEN = 'be874f46-5e1e-431b-85f2-2d1a1e229a82';
+export const EXTERNAL_API_BASE_URL = 'https://backoffice.applebroker.io/api/v1';
+export const REGISTER_TOKEN = 'dfb6c6a5-6889-4856-992e-df328a8ddf9a';
