b03385144a00058593ac49b2040462e0d453ab63
Lucas Carvalho <barraroot@gmail.com>
2025-06-10 08:18:54 -0300

    Deploy Vercel

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,3 +1,4 @@
 // Constantes da API externa
 export const EXTERNAL_API_BASE_URL = 'https://backoffice.applebroker.io/api/v1';
 export const REGISTER_TOKEN = 'dfb6c6a5-6889-4856-992e-df328a8ddf9a';
+
