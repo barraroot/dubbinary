3dccf7e00796a1c209aeaa7d8e8ec757a5630d16
Daniel Oliveira <38472540+DanOliveira98@users.noreply.github.com>
2025-06-13 14:35:29 -0300

    Fixed url

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,3 +1,3 @@
 // Constantes da API externa
-export const EXTERNAL_API_BASE_URL = 'https://backoffice.asafebroker.com/api';
+export const EXTERNAL_API_BASE_URL = 'https://backoffice-ob.flowsys.com.br/api';
 export const REGISTER_TOKEN = '514afe68-4685-4da1-86b7-dbe4c8e6a7a4';
