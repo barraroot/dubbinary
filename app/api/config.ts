c9aa79b682cf872ef3dfee76f07c249bb3b86c5f
Lucas Carvalho <barraroot@gmail.com>
2025-06-15 09:25:01 -0300

    applebroker Ajuste

--- app/api/config.ts
+++ app/api/config.ts
@@ -1,3 +1,3 @@
 // Constantes da API externa
-export const EXTERNAL_API_BASE_URL = 'https://backoffice-ob.flowsys.com.br/api';
+export const EXTERNAL_API_BASE_URL = 'https://backoffice.applebroker.io/api';
 export const REGISTER_TOKEN = 'dfb6c6a5-6889-4856-992e-df328a8ddf9a';
